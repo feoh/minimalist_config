5d5cf7d0c3ba6054b68beb57d205135cf1f21d75
Christopher Patti <feoh@feoh.org>
2025-10-12 20:01:31 -0400

    Add typescript tools.

---
+++ lua/proglangs.lua
@@ -0,0 +1,5 @@
+-- Typescript
+
+vim.pack.add("https://github.com/nvim-lua/plenary.nvim")
+vim.poack.add("https://github.com/neovim/nvim-lspconfig")
+vim.pack.add("https://github.com/pmizio/typescript-tools.nvim")
