9ef8d4672780545a22d0657ad08ec41939e667d4
Christopher Patti <feoh@feoh.org>
2025-10-13 22:39:23 -0400

    remove single line only mode for virt diags

--- lua/diags.lua
+++ lua/diags.lua
@@ -1,7 +1,6 @@
 -- Use the swank new virtual lines diagnostics option!
 vim.diagnostic.config({
   virtual_lines = {
-    only_current_line = true,  -- Show virtual lines for all diagnostics, not just the current line
     highlight_whole_line = true,  -- Highlight the whole line with the diagnostic
     prefix = "▎",               -- Character to use as prefix for virtual lines
     spacing = 4,                -- Number of empty spaces between prefix and diagnostic message
