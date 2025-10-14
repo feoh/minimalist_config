a37b790486297666606412c5424fefc19a6bd94a
Christopher Patti <feoh@feoh.org>
2025-10-13 22:45:06 -0400

    try to turn off diag spam

--- lua/diags.lua
+++ lua/diags.lua
@@ -1,7 +1,7 @@
 -- Use the swank new virtual lines diagnostics option!
 vim.diagnostic.config({
   virtual_lines = {
-    highlight_whole_line = true,  -- Highlight the whole line with the diagnostic
+    highlight_whole_line = false,  -- Highlight the whole line with the diagnostic
     prefix = "▎",               -- Character to use as prefix for virtual lines
     spacing = 4,                -- Number of empty spaces between prefix and diagnostic message
   }
