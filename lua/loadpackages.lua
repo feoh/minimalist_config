ea5a9d0079bdf7c023b9d54f5575c8a63c6a4300
Christopher Patti <feoh@feoh.org>
2025-10-16 01:55:43 -0400

    forgot loadpackages

---
+++ lua/loadpackages.lua
@@ -0,0 +1,20 @@
+-- theme
+vim.pack.add({'https://github.com/catppuccin/nvim.git'})
+-- lualine
+vim.pack.add({'https://github.com/nvim-tree/nvim-web-devicons'})
+vim.pack.add({'https://github.com/nvim-lualine/lualine.nvim'})
+
+vim.pack.add({'https://github.com/neovim/nvim-lspconfig'})
+vim.pack.add({'https://github.com/folke/which-key.nvim.git'})
+vim.pack.add({{src='https://github.com/Saghen/blink.cmp.git', version="v1.7.0"}})
+vim.pack.add({'https://github.com/stevearc/oil.nvim.git'})
+
+-- Typescript
+vim.pack.add({"https://github.com/nvim-lua/plenary.nvim"})
+vim.pack.add({"https://github.com/pmizio/typescript-tools.nvim"})
+
+
+
+
+-- Done specifying packages. Update them all!
+vim.pack.update(nil, {force=true})
