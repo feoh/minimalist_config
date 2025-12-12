0be084bfa8c4d5f069ec3be46ac781149e9e3b65
Christopher Patti <feoh@feoh.org>
2025-12-11 19:41:23 -0500

    fix: Fix boolean option syntax

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,6 +1,6 @@
 -- set wrapmargin=120
 -- set wrap
 -- set linebreak
-vim.o.wrap=120
 vim.o.textwidth=120
+vim.o.wrap
 
--- ftplugin/rst.lua
+++ ftplugin/rst.lua
@@ -1,5 +1,5 @@
 -- set ts=3 sts=3 sw=3 expandtab
-vim.o.wrap=120
+vim.o.wrap
 vim.o.textwidth=120
 vim.o.ts=3
 vim.o.sts=3
